db67eb2e7b0a18e3b2b801b21cfb708d3bdff355
Luis Escobar <lae311@stern.nyu.edu>
2022-06-17 13:51:03 -0400

    listcreate django

--- backend/products/views.py
+++ backend/products/views.py
@@ -2,10 +2,21 @@ from rest_framework import generics
 from .models import Product
 from .serializers import ProductSerializer
 
-class ProductCreateAPIView(generics.CreateAPIView):
+class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-product_create_view = ProductCreateAPIView.as_view()
+    
+    def perform_create(self, serializer):
+        # serializer.save(user = self.request.user)
+        print(serializer.validated_data)
+        title = serializer.validated_data.get('title')
+        content = serializer.validated_data.get('content') or None
+        if content is None:
+            content =title
+        serializer.save(content = content)
+        # send a Django signal
+        
+product_list_create_view = ProductListCreateAPIView.as_view()
 
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
--- py_client/detail.py
+++ py_client/detail.py
@@ -1,9 +1,10 @@
 import requests
-
+import random
 endpoint = "http://localhost:8000/api/products/"
 
 data = {
-    'title': "This field is done"
+    'title': "This field is done",
+    'price': random.randint(0, 10)
 }
 
 get_response = requests.post(endpoint, json=data)
